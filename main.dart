6646108882f457c33ea84e08bbb98ee2b1a397f2
gitusser-coder <p1234ppm@yahoo.com>
2025-05-14 19:23:59 +0200

    Update main.dart
    layout, grundgerüst und bild wurden eingebettet

--- main.dart
+++ main.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bildbearbeitung',
+      home: Mintano(),
+    );
+  }
+}
+
+
+class Mintano extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(title: Text('Bildbearbeitung')), 
+      body: Center( 
+        child: Image.asset(
+          'assets/gojo_zeigt_bg.png',
+          width: 300,
+        ), //Image.asset, Bild anzeigen 
+      ), // Center, Inhalt 
+    ); //Scafold, Layout 
+  }
+}
